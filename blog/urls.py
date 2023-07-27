de87ef18a6a9f159bc8cb499219205e65a3d38da
xudayfi <xudayfiibraahim8@gmail.com>
2023-07-27 14:49:45 +0300

    feching data from the database and working the urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.homePage)
+    path('', views.homePage, name='home'),
+    path('categories', views.CategoryViewList, name='category')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
-
+from . models import Category
 # Home page
 def homePage(request):
     return render(request, 'blog/index.html')
+
+
+# Category view
+def CategoryViewList(request):
+    cat = Category.objects.order_by('-id')
+    context = {
+        'categories': cat
+    }
+    return render(request, 'blog/category.html', context)
