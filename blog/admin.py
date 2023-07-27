edff33a43c981886c757b71241d57d7f9e0d2945
xudayfi <xudayfiibraahim8@gmail.com>
2023-07-27 14:13:46 +0300

    finishing the models remaing things

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Post
 
 
 #Category
-
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_date', 'created_by')
+    list_display  = ('name', 'created_by', 'created_date')
 
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
+
+
+#Post
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'category', 'content', 'published', 'tags', 'created_by')
+    prepopulated_fields = {"slug":("title",)}
+
+
+admin.site.register(Post,PostAdmin)
+
+
 
--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,23 @@ class Category(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     created_by = models.IntegerField()
     
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # image = models.ImageField(upload_to='static/images')
+    content = models.TextField()
+    published = models.BooleanField(default=False)
+    tags = models.CharField(max_length=30)
+    created_by = models.IntegerField()
+    
+    # def __str__ (self):
+    #     return self.title
+    
